5b1b39c90a8291e64ca47898b5401981e9a9ded2
Melisa Carrizo <melisacarrizo5@gmail.com>
2023-05-23 17:04:30 -0300

    arreglos

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 console.log("Bienvenido a Alquileres Pepito")
 
 let noches = "";
-let _menores = "";
+let menores = "";
 let total_inicial = 5000;
-let _seña = Number;
+let seña = Number;
 
-function calcular_total ( noches , adultos , _menores ){
+function calcular_total ( noches , adultos , menores ){
     noches = parseInt(noches);
     adultos = parseInt(adultos);
-    _menores = parseInt(_menores);
+    menores = parseInt(menores);
 
-    if (adultos + _menores> 7 ){
+    if (adultos + menores> 7 ){
         console.log("La cantidad total de adultos y menores supera el limite (7)");
     }
-    else if (adultos + _menores <= 7) {
-        total = (noches * total_inicial) + (adultos * 600) + (_menores * 300);
+    else if (adultos + menores <= 7) {
+        total = (noches * total_inicial) + (adultos * 600) + (menores * 300);
         return total;
     }
-    else if (adultos <= 0 && _menores <=0 ) {
+    else if (adultos <= 0 && menores <=0 ) {
         console.log("Los datos ingresados son incorrectos");
     }
 }
@@ -25,15 +25,15 @@ function calcular_total ( noches , adultos , _menores ){
 while ( noches != "FIN") {
     noches = prompt("Ingrese la cantidad de noches que desea o ingrese FIN para finalizar");
     if ( noches != "FIN") {
-        let _menores = prompt("Ingrese la cantidad de personas menores a 3 años")
+        let menores = prompt("Ingrese la cantidad de personas menores a 3 años")
         let adultos = prompt("Ingrese la cantidad de personas mayores a 3 años")
-        let total = calcular_total ( noches , adultos , _menores );
-        let _seña = total / 3 ;  
+        let total = calcular_total ( noches , adultos , menores );
+        let seña = total / 3 ;  
         console.log("Noches:", noches)
-        console.log("Cantidad de menores:" , _menores)
+        console.log("Cantidad de menores:" , menores)
         console.log("Cantidad de adultos:" , adultos)
         console.log("Total de la reserva:" , total)
-        console.log("Valor de la seña:", _seña)
+        console.log("Valor de la seña:", seña)
     }
 }
 
